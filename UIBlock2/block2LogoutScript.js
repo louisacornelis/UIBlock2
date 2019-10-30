24ffbb0c0bacdc9a7051bab87fa6591becbcd7bf
Toon <toonverm@gmail.com>
2019-10-30 20:53:46 +0100

    blabla

--- UIBlock2/block2LogoutScript.js
+++ UIBlock2/block2LogoutScript.js
@@ -1,168 +1,172 @@
 var accountContainer;
 
 function logout() {
-    document.cookie = "Logged=out";
+    document.cookie = 'Logged=out';
     accountInitOnLoad();
 }
 
 function login(e) {
-    console.log("test");
+    console.log('test');
     e.preventDefault();
-    let emailel = document.getElementById("loginEmail");
-    let passwordel = document.getElementById("loginPw");
+    let emailel = document.getElementById('loginEmail');
+    let passwordel = document.getElementById('loginPw');
     let email = emailel.value;
     let password = passwordel.value;
     let cookie = getCookie(email);
     if (cookie) {
         let userInfo = JSON.parse(cookie);
         if (userInfo.password === password) {
-            emailel.value = "";
-            passwordel.value = "";
+            emailel.value = '';
+            passwordel.value = '';
             setCookieLogged(email);
             accountInitOnLoad();
         } else {
-            alert("Incorrect Password");
+            alert('Incorrect Password');
         }
     } else {
-        alert("Unregistered Email");
+        alert('Unregistered Email');
     }
 
 }
 
 function register() {
     hide();
-    var registerInterface = document.getElementById("registerContainer");
-    registerInterface.style.display = "flex";
+    var registerInterface = document.getElementById('registerContainer');
+    registerInterface.style.display = 'flex';
 }
 
 function displayLogIn() {
     hide();
-    var logInInterface = document.getElementById("logInContainer");
-    logInInterface.style.display = "flex";
+    var logInInterface = document.getElementById('logInContainer');
+    logInInterface.style.display = 'flex';
 }
 
 function displayMainContainer() {
     hide();
-    var mainContainer = document.getElementById("mainContainer");
-    mainContainer.style.display = "flex";
+    var mainContainer = document.getElementById('mainContainer');
+    mainContainer.style.display = 'flex';
 }
 
 function hide() {
-    var mainContainer = document.getElementById("mainContainer");
-    mainContainer.style.display = "none";
-    var logInInterface = document.getElementById("logInContainer");
-    logInInterface.style.display = "none";
-    var registerInterface = document.getElementById("registerContainer");
-    registerInterface.style.display = "none";
+    var mainContainer = document.getElementById('mainContainer');
+    mainContainer.style.display = 'none';
+    var logInInterface = document.getElementById('logInContainer');
+    logInInterface.style.display = 'none';
+    var registerInterface = document.getElementById('registerContainer');
+    registerInterface.style.display = 'none';
 }
 
 window.onload = function () {
+    let cookie = getCookie('Logged');
+    if (!cookie) {
+        setCookieLogged('out');
+    }
     dateTimeOnload();
     accountInitOnLoad();
 };
 
 function accountInitOnLoad() {
-    loginContainer = document.getElementById("LoginButton");
-    registerContainer = document.getElementById("registerButton");
-    let logged = getCookie("Logged");
-    let mainContainer = document.getElementById("mainContainer");
-    loginContainer.innerHTML = "";
-    registerContainer.innerHTML = "";
-    if (logged !== "out") {
+    loginContainer = document.getElementById('LoginButton');
+    registerContainer = document.getElementById('registerButton');
+    let logged = getCookie('Logged');
+    let mainContainer = document.getElementById('mainContainer');
+    loginContainer.innerHTML = '';
+    registerContainer.innerHTML = '';
+    if (logged !== 'out') {
         displayMainContainer();
-        console.log("logged in");
-        let src = localStorage.getItem("IMG_" + getCookie("Logged"));
-        mainContainer.style.visibility = "unset";
-        var logOutEl = document.createElement("div");
-        var profile = document.createElement("div");
-        var image = document.createElement("img");
-        var name = document.createElement("p");
+        console.log('logged in');
+        let src = localStorage.getItem('IMG_' + getCookie('Logged'));
+        mainContainer.style.visibility = 'unset';
+        var logOutEl = document.createElement('div');
+        var profile = document.createElement('div');
+        var image = document.createElement('img');
+        var name = document.createElement('p');
         let user = JSON.parse(getCookie(logged));
         name.innerText = user.username;
-        image.src = src ? src : "default-profile-picture.png";
-        image.classList = "profile";
-        profile.classList = "round menuItem logOut";
+        image.src = src ? src : 'default-profile-picture.png';
+        image.classList = 'profile';
+        profile.classList = 'round menuItem logOut';
         profile.appendChild(image);
         profile.appendChild(name);
-        logOutEl.innerText = "Log Out";
-        logOutEl.classList = "round menuItem logOut";
+        logOutEl.innerText = 'Log Out';
+        logOutEl.classList = 'round menuItem logOut';
         loginContainer.appendChild(logOutEl);
-        logOutEl.addEventListener("click", logout);
+        logOutEl.addEventListener('click', logout);
         registerContainer.appendChild(profile);
     } else {
-        console.log("logged out");
-        mainContainer.style.visibility = "hidden";
-        var register = document.createElement("div");
-        register.innerText = "Register";
-        register.classList = "round menuItem logOut";
+        console.log('logged out');
+        mainContainer.style.visibility = 'hidden';
+        var register = document.createElement('div');
+        register.innerText = 'Register';
+        register.classList = 'round menuItem logOut';
         registerContainer.appendChild(register);
-        registerContainer.addEventListener("click", register);
+        registerContainer.addEventListener('click', register);
 
-        var logIn = document.createElement("div");
-        logIn.innerText = "Log In";
-        logIn.classList = "round menuItem logOut";
+        var logIn = document.createElement('div');
+        logIn.innerText = 'Log In';
+        logIn.classList = 'round menuItem logOut';
         loginContainer.appendChild(logIn);
         logIn.addEventListener('click', displayLogIn);
     }
 }
 
 function setCookieLogged(email) {
-    document.cookie = "Logged=" + email;
+    document.cookie = 'Logged=' + email;
 }
 
 function setCookieUser(userInfo) {
     setCookieLogged(userInfo.email);
-    document.cookie = userInfo.email + "=" + JSON.stringify(userInfo);
+    document.cookie = userInfo.email + '=' + JSON.stringify(userInfo);
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(";");
+    var name = cname + '=';
+    var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == " ") {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
-    return "";
+    return '';
 }
 
 function save(e) {
     e.preventDefault();
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let name = document.getElementById("name").value;
-    let surname = document.getElementById("surname").value;
-    let email = document.getElementById("email").value;
-    let birthday = document.getElementById("birthday").value;
-    let interest = getAllSelected(document.getElementById("interest"));
-    let language = getAllSelected(document.getElementById("language"));
-    let purpose = document.getElementById("purpose").value;
-    let image = document.getElementById("inputFile");
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let name = document.getElementById('name').value;
+    let surname = document.getElementById('surname').value;
+    let email = document.getElementById('email').value;
+    let birthday = document.getElementById('birthday').value;
+    let interest = getAllSelected(document.getElementById('interest'));
+    let language = getAllSelected(document.getElementById('language'));
+    let purpose = document.getElementById('purpose').value;
+    let image = document.getElementById('inputFile');
     readURL(email, image);
     if (getCookie(email)) {
-        alert("This email is already registered.");
+        alert('This email is already registered.');
     } else {
         userInfo = {
-            "username": username,
-            "password": password,
-            "name": name,
-            "surname": surname,
-            "email": email,
-            "birthday": birthday,
-            "interest": interest,
-            "language": language,
-            "purpose": purpose
+            'username': username,
+            'password': password,
+            'name': name,
+            'surname': surname,
+            'email': email,
+            'birthday': birthday,
+            'interest': interest,
+            'language': language,
+            'purpose': purpose
         };
         console.log(JSON.stringify(userInfo));
         setCookieUser(userInfo);
         accountInitOnLoad();
     }
-    var frm = document.getElementById("register");
+    var frm = document.getElementById('register');
     frm.reset();
 }
 
@@ -189,7 +193,7 @@ function readURL(email, input) {
         var reader = new FileReader();
         reader.onload = function (e) {
             console.log(e.target.result);
-            localStorage.setItem("IMG_" + email, e.target.result);
+            localStorage.setItem('IMG_' + email, e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
     }
